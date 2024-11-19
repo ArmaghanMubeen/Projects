657144207a700e31a05603791fa01fe765cc6540
armaghan <developer5264@gmail.com>
2024-11-19 16:21:12 +0500

    routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,66 @@
 import React, { useEffect, useRef } from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
-const App = () => {
+const AppStartTop = () => {
+  return (
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
+      </div>
+      <About />
+      <Experience />
+      <Tech />
+      <Works />
+      <Feedbacks />
+      <div className='relative z-0'>
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+  );
+};
+
+const AppStartProjects = () => {
   const worksRef = useRef(null);
 
   useEffect(() => {
-    // Scroll to the Works section on load
     worksRef.current?.scrollIntoView({ behavior: "smooth" });
   }, []);
 
   return (
-    <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
-          <Navbar />
-          <Hero />
-        </div>
-        <About />
-        <Experience />
-        <Tech />
-        {/* Add a ref to the Works section */}
-        <div ref={worksRef}>
-          <Works />
-        </div>
-        <Feedbacks />
-        <div className='relative z-0'>
-          <Contact />
-          <StarsCanvas />
-        </div>
+    <div className='relative z-0 bg-primary'>
+      <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+        <Navbar />
+        <Hero />
       </div>
+      <About />
+      <Experience />
+      <Tech />
+      {/* Add a ref to the Works section */}
+      <div ref={worksRef}>
+        <Works />
+      </div>
+      <Feedbacks />
+      <div className='relative z-0'>
+        <Contact />
+        <StarsCanvas />
+      </div>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<AppStartTop />} />
+        <Route path="/projects" element={<AppStartProjects />} />
+      </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,4 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/Projects/", // Base path as specified in the homepage URL
 });
